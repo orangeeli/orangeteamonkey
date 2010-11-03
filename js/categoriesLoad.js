970a8067c4bdf35a9f6286814cbe449717bb04b0
orangeeli <eliseu.martinho@gmail.com>
2010-11-03 19:57:29 +0000

    setting variables namesin a consistent way

--- js/categoriesLoad.js
+++ js/categoriesLoad.js
@@ -12,38 +12,38 @@ function loadCategories() {
                  dataType: "xml",
                  beforeSend: function(){
 		     //alert('teste 1!');
-                     $('#DivCategories').html('');
-                     //$('#LoaderImg').attr('display', 'inline');
-		     $('#LoaderImg').html('<img src=\"/images/loader.gif\" alt=\"Loading Categories\" />');
-		     //alert('teste 3! ' + $('#LoaderImg').attr('display'));
+                     $('#div_categories').html('');
+                     //$('#loader_img').attr('display', 'inline');
+		     $('#loader_img').html('<img src=\"/images/loader.gif\" alt=\"Loading Categories\" />');
+		     //alert('teste 3! ' + $('#loader_img').attr('display'));
                  },
                  success: function(xml) {
 		     //alert('teste 2');
-		     $('#DivCategories').html('<ul id=\"Categories\"></ul>');
+		     $('#div_categories').html('<ul id=\"categories\"></ul>');
                      $(xml).find('category').each(function(){
                          var id_text = $(this).attr('id')
                          var name_text = $(this).find('name').text();
 
-			 /*<span class=\"liTextColor\">*/
+			 /*<span class=\"li_text_color\">*/
                          $('<li></li>')
                              .html('<a href=\"/archive.html#'+name_text+'\" title=\"'+name_text+'\">'+name_text+'</a>')
-                             .appendTo('#Categories');
+                             .appendTo('#categories');
                      }); //close each(
                  },
                  error: function(){
 		     //alert('Erro');
-                     $('#DivCategories').html('<span>The Categories list couldn\'t be loaded. Please <a href=\"#\" id="ReloadCategories">try</a> again later.</span>');
+                     $('#div_categories').html('<span>The Categories list couldn\'t be loaded. Please <a href=\"#\" id="reload_categories">try</a> again later.</span>');
                  },
                  complete: function(){
-                     //$('#LoaderImg').attr('display', 'none');
-		     $('#LoaderImg').html('');
-		     var reloadAnchor = $('#ReloadCategories');
+                     //$('#loader_img').attr('display', 'none');
+		     $('#loader_img').html('');
+		     var reloadAnchor = $('#reload_categories');
 		     if(reloadAnchor)
 		     	reloadAnchor.click(function(){loadCategories();});
                  }
              }); //close $.ajax(
 
-	 //$('#ReloadCategories').click(loadCategories());
+	 //$('#reload_categories').click(loadCategories());
 
 }
 
